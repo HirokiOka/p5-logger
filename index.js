255102e96a09f2ac42ab8da17146739c6c995802
hiroki <hiro_bass.0827@icloud.com>
2021-11-11 12:56:54 +0900

    refactor

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-console.log('loaded');
-/*
-const script = document.createElement('script');
-script.src = 'https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.3/codemirror.min.js';
-document.head.appendChild(script);
-*/
+console.log('p5-logger loaded');
+
+const serverURL = 'http://localhost:3000/data';
+const buttonClass = 'toolbar__play-button';
 
 async function fetchPost(data) {
-  const url = 'http://localhost:3000/data';
   const options = {
     headers: {
         'Accept': 'application/json',
@@ -15,45 +12,28 @@ async function fetchPost(data) {
     body: JSON.stringify(data),
     method: "POST",
   };
-
-  let res = await fetch(url, options);
+  let res = await fetch(serverURL, options);
   return res.json();
 }
 
-const buttonClass = 'toolbar__play-button';
 document.addEventListener('click', (e) => {
-  try {
-    if (e.target.className.split(' ')[0] === buttonClass) {
-      const code = document.querySelector('.CodeMirror').textContent;
-      const time = new Date();
-      const postData = {
-        time,
-        code
-      };
-      fetchPost(postData).then(res => {
-        console.log(res.message);
-      });
+  if (e.target.className === undefined) return;
+
+    if (typeof(e.target.className) === 'object') {
+      if ((e.path[2].className !== undefined) && (e.path[2].className.split(' ')[0] === buttonClass)) {
+          const code = document.querySelector('.CodeMirror').textContent;
+          const time = new Date();
+          const postData = { time, code };
+          console.log(postData);
+          //fetchPost(postData).then(res => { console.log(res.message); });
+      } 
+    } else if (typeof(e.target.className) === 'string') {
+      if (e.target.className.split(' ')[0] === buttonClass) {
+        const code = document.querySelector('.CodeMirror').textContent;
+        const time = new Date();
+        const postData = { time, code };
+        console.log(postData);
+        //fetchPost(postData).then(res => { console.log(res.message); });
+      }
     }
-  } catch (err) {
-  }
-
-  try {
-    if (e.path[2].className.split(' ')[0] === buttonClass) {
-      const code = document.querySelector('.CodeMirror').textContent;
-      const time = new Date();
-      const postData = {
-        time,
-        code
-      };
-
-      fetchPost(postData).then(res => {
-        console.log(res.message);
-      });
-    }
-  } catch (err) {
-    conosle.log(err);
-  }
-
 });
-
-
